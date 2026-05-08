735933a3b930147d4f5acdb41042408de979d482
Juan José Restrepo <juanrestrp.val@gmail.com>
2026-05-08 17:28:25 -0500

    Update mod.rs

--- terapia-visual/terapia_visual_domain/src/domain/mod.rs
+++ terapia-visual/terapia_visual_domain/src/domain/mod.rs
@@ -1,3 +1,4 @@
+pub mod app_settings;
 pub mod color;
 pub mod geometry;
 pub mod layout;
@@ -5,6 +6,7 @@ pub mod opacity;
 pub mod therapy_config;
 pub mod zone;
 
+pub use app_settings::AppSettings;
 pub use color::Color;
 pub use geometry::ZoneRect;
 pub use layout::Layout;
